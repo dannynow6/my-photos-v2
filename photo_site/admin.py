9c58f9601621fd91a79d95234c609121a202957b
Dan Garro <dobetterwithdan@gmail.com>
2023-05-21 21:35:07 -0400

    moved alert for error to top of page, included close dialogue button

--- photo_site/admin.py
+++ photo_site/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Photo, Comment
+from .models import Photo, Comment, Lens
 
 # Register your models here.
 admin.site.register(Photo)
 admin.site.register(Comment)
+admin.site.register(Lens) 
