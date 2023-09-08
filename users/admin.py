775263302ed754944916fb9506680e1b053693f6
Dan Garro <dobetterwithdan@gmail.com>
2023-09-08 19:04:22 -0400

    fixed problem with uploading img via admin

--- users/admin.py
+++ users/admin.py
@@ -7,14 +7,20 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "age")
 
     def save_model(self, request, obj, form, change):
-        # check if a new image has been uploaded
-        if "photo" in form.changed_data:
-            profile_photo = form.cleaned_data["photo"]
-            processed_image = process_image(profile_photo)
-            obj.photo = processed_image
+        try:
+            # check if a new image has been uploaded
+            if "photo" in form.changed_data:
+                profile_photo = form.cleaned_data["photo"]
+                if hasattr(profile_photo, "read"):
+                    processed_image = process_image(profile_photo)
+                    obj.photo = processed_image
+                else:
+                    print("Invalid file received", profile_photo)
 
-        # save the model
-        super().save_model(request, obj, form, change)
+            # save the model
+            super().save_model(request, obj, form, change)
+        except Exception as e:
+            print("Error in save_model:", e)
 
 
 # Register your models here.
